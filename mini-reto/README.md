5fdfaca6fc43cec3ed574ed361cc7aee0fe3a313
Larissa <dejamepienso@gmail.com>
2019-11-09 11:19:54 -0600

    Add websockets with vue

--- mini-reto/README.md
+++ mini-reto/README.md
@@ -10,15 +10,5 @@ npm install
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
 ### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+ This need to be running in local  :8081, and docker in :8080
--- mini-reto/src/App.vue
+++ mini-reto/src/App.vue
@@ -6,7 +6,23 @@
           <h1>Estatus de Ruta</h1>
         </div>
         <div class="row">
-          <RouteCard></RouteCard>
+          <button class="button" @click="connect" v-if="status === 'disconnected'">Mostrar estatus</button>
+          <button class="button" @click="disconnect" v-if="status === 'connected'">Terminar</button>
+        </div>
+        <div class="row">
+          <hr />
+        </div>
+        <div class="row">
+          <div class="table">
+            <div class="table__wrapper">
+              <RouteCard
+                v-bind:key="log.index"
+                v-for="log in logs"
+                :status="log.status"
+                :class="log.status === 'completed' ? 'done' : 'progress'"
+              ></RouteCard>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -14,12 +30,34 @@
 </template>
 
 <script>
-import RouteCard from "./components/RouteCard.vue";
+import RouteCard from "./components/RouteCard.vue"
 
 export default {
   name: "app",
+  data() {
+    return {
+      logs: [],
+      status: "disconnected"
+    }
+  },
   components: {
     RouteCard
+  },
+  methods: {
+    connect() {
+      this.socket = new WebSocket("ws://localhost:8080")
+      this.socket.onopen = () => {
+        this.status = "connected"
+        this.socket.onmessage = ({ data }) => {
+          this.logs.push(JSON.parse(data))
+        }
+      }
+    },
+    disconnect() {
+      this.socket.close()
+      this.status = "disconnected"
+      this.logs = []
+    }
   }
-};
+}
 </script>
